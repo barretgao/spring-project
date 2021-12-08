055c6e8a00c34914f26903e4f92131474a2d9abc
barretgao <yipu.gao@vanderbilt.edu>
2021-12-07 19:42:07 -0500

    all modification and added file

--- code/keyboard_inputs.py
+++ code/keyboard_inputs.py
@@ -9,36 +9,84 @@ from directkeys import PressKey, ReleaseKey, W, A, S, D
 import pyautogui
 
 
-def roi(img, vertices):
+
+
+def draw_lines(img,lines):
+    if lines is not None:
+        for line in lines:
+            coords = line[0]
+            cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255,255,255], 3)
+
+
+def region(img):
     mask = np.zeros_like(img)
-    cv2.fillPoly(mask, vertices, 255)
+    vertices = np.array([[0,600],[0,400], [200,200], [600,200], [800,400], [800,600], [500, 600], [500, 300], [300, 300], [300, 600], [0,600]], np.int32)
+    cv2.fillPoly(mask, [vertices], 255)
     masked = cv2.bitwise_and(img, mask)
     return masked
 
-def draw_lines(image, lines):
-    try:
+def average(image, lines):
+    left =[]
+    right =[]
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        y_int = parameters[1]
+        if slope < 0:
+            left.append((slope, y_int))
+        else:
+            right.append((slope, y_int))
+    right_avg = np.average(right, axis=0)
+    left_avg = np.average(left, axis=0)
+    left_line = make_points(image, left_avg)
+    right_line = make_points(image, right_avg)
+    return np.array([left_line, right_line])
+
+def make_points(image, average):
+    slope, y_int = average 
+    y1 = image.shape[0]
+    y2 = int(y1 * (3/5))
+    x1 = int((y1 - y_int) // slope)
+    x2 = int((y2 - y_int) // slope)
+    return np.array([x1, y1, x2, y2])
+
+def display_lines(image, lines):
+    if lines is not None:
         for line in lines:
             coords = line[0]
-            cv2.line(image, (coords[0],coords[1]), (coords[2],coords[3]), [255,255,255], 3)
-    except:
-        pass
+            cv2.line(image, (coords[0], coords[1]), (coords[2], coords[3]), [255,255,255], 3)
 
-def process_img(image):
-    original_image = image
-    # convert to gray
-    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # edge detection
-    processed_img =  cv2.Canny(processed_img, threshold1 = 100, threshold2=300)
-    #blur the line with gaussianBlur
-    processed_img = cv2.GaussianBlur(processed_img, (5,5), 0)
-    # find the region of interests
-    vertices = np.array([[0,750],[0,500], [400,400], [700,400], [1024,500], [1024,750]], np.int32)
-    processed_img = roi(processed_img, [vertices])
+def record_screen(image):
+    #copy of image
+    raw_image = np.copy(image)
 
-    #get the edges. 
-    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 100, 15)
-    draw_lines(processed_img, lines)
-    return processed_img
+    #change color to gray so that we can use canny edge detector.      
+    screen = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+    #reduce the size of the input frame. 
+    
+
+    #use gaussian blur to remove noises
+    screen = cv2.GaussianBlur(screen,(5,5),0)
+
+    # # edge detection
+    screen =  cv2.Canny(screen, 80, 200)
+        
+    # #get partial screen. 
+    screen = region(screen)
+
+    #get lines
+    #lines = cv2.HoughLinesP(screen, 2, np.pi/180, 100, np.array([]), minLineLength=30, maxLineGap=15)
+
+    #draw_lines(screen, lines)
+    
+    # average_line = average(raw_image, lines)
+        
+    # display_lines(raw_image, lines)
+
+    return screen, raw_image
+
 
 def main():
     
@@ -46,8 +94,9 @@ def main():
         print(i+1)
         time.sleep(1)
     while True:
-        screen = grab_screen(region=(0,40,1024,768))
-        new_screen = process_img(screen)
+        screen = grab_screen(region=(0,40,800,600))
+        new_screen, raw_image = record_screen(screen)
+        #new_screen = cv2.resize(new_screen, (80, 60))
         cv2.imshow('window', new_screen)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
